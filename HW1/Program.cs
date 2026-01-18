d43b27059180589d185a027faf7b605db7f2ba30
Xanonblade <116854916+Xanonblade@users.noreply.github.com>
2026-01-18 10:22:50 -0500

    Create file

---
+++ HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Laurin Zanon
+//Corey Stinson
+//Sly Cash
+//Lucas Kishore
+//1/18/26
+namespace HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
