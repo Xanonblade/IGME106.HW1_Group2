e47c4b3c89c17a88b6d337510485af4998104524
Corey Stinson <corey@coreystinson.com>
2026-01-18 11:14:57 -0500

    Added error handling for storyteller file reading

--- HW1/Storyteller.cs
+++ HW1/Storyteller.cs
@@ -26,49 +26,79 @@ namespace HW1
         /// </summary>
         public void ReadFile()
         {
-            StreamReader sr = new StreamReader("../../../Setting.txt");
+            StreamReader sr;
             string line = "";
-
-            // Read Settings
-            while ((line = sr.ReadLine()!) != null)
+            try
             {
-                string location = line.Substring(0, line.IndexOf('|'));
-                string timePeriod = line.Substring(line.IndexOf('|') + 1);
-                settings.Add(new Setting(location, timePeriod));
-            }
-            sr.Close();
+                sr = new StreamReader("../../../Setting.txt");
 
-            sr = new StreamReader("../../../Actor.txt");
-            // Read Actors
-            while ((line = sr.ReadLine()!) != null)
+                // Read Settings
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    string location = line.Substring(0, line.IndexOf('|'));
+                    string timePeriod = line.Substring(line.IndexOf('|') + 1);
+                    settings.Add(new Setting(location, timePeriod));
+                }
+                sr.Close();
+            }
+            catch (Exception e)
             {
-                string name = line.Substring(0, line.IndexOf('|'));
-                line = line.Substring(line.IndexOf('|') + 1);
-                string age = line.Substring(0, line.IndexOf('|'));
-                line = line.Substring(line.IndexOf('|') + 1);
-                string profession = line;
-
-                actors.Add(new Actor(name, profession, age));
+                Console.WriteLine("Error reading Setting: " + e.Message);
             }
-            sr.Close();
-
-            sr = new StreamReader("../../../Conflict.txt");
-            // Read conflict
-            while ((line = sr.ReadLine()!) != null)
+            
+            try
             {
-                conflicts.Add(new Conflict(line));
-            }
-            sr.Close();
+                sr = new StreamReader("../../../Actor.txt");
+                // Read Actors
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    string name = line.Substring(0, line.IndexOf('|'));
+                    line = line.Substring(line.IndexOf('|') + 1);
+                    string age = line.Substring(0, line.IndexOf('|'));
+                    line = line.Substring(line.IndexOf('|') + 1);
+                    string profession = line;
 
-            sr = new StreamReader("../../../Resolution.txt");
-            // Read Resolution
-            while ((line = sr.ReadLine()!) != null)
-            {
-                string type = line.Substring(0, line.IndexOf('|'));
-                string solution = line.Substring(line.IndexOf('|') + 1);
-                resolutions.Add(new Resolution(type, solution));
+                    actors.Add(new Actor(name, profession, age));
+                }
+                sr.Close();
             }
-            sr.Close();
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading Actor: " + e.Message);
+            }
+
+            try
+            {
+                sr = new StreamReader("../../../Conflict.txt");
+                // Read conflict
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    conflicts.Add(new Conflict(line));
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading Conflict: " + e.Message);
+            }
+
+            try
+            {
+                sr = new StreamReader("../../../Resolution.txt");
+                // Read Resolution
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    string type = line.Substring(0, line.IndexOf('|'));
+                    string solution = line.Substring(line.IndexOf('|') + 1);
+                    resolutions.Add(new Resolution(type, solution));
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading Resolution: " + e.Message);
+            }
+
         }
 
         /// <summary>
